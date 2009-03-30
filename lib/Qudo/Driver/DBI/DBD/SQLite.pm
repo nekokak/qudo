ec64dacb305a5e11ff50388a447456b7ac84b4db
Atsushi, Kobayashi <nekokak@gmail.com>
2009-03-30 17:30:47 +0900

    masartz--

--- lib/Qudo/Driver/DBI/DBD/SQLite.pm
+++ lib/Qudo/Driver/DBI/DBD/SQLite.pm
@@ -6,19 +6,5 @@ sub last_insert_id { $_[1]->func('last_insert_rowid') }
 
 sub sql_for_unixtime { return time() }
 
-sub bulk_insert {
-    my ($skinny, $table, $args) = @_;
-
-    $skinny->dbh->begin_work;
-
-        for my $arg ( @{$args} ) {
-            $skinny->insert($table, $arg);
-        }
-
-    $skinny->dbh->commit;
-
-    return 1;
-}
-
 1;
 
