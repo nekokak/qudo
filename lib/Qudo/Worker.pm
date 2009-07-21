5f52760fc650766ff3c610cd37da45a37f80929e
Masaru Hoshino <masartz@gmail.com>
2009-07-21 22:36:26 +0900

    add Worker's POD

--- lib/Qudo/Worker.pm
+++ lib/Qudo/Worker.pm
@@ -33,4 +33,51 @@ sub work_safely {
     return $res;
 }
 
+=head1 NAME
+
+Qudo::Worker - superclass for defining task behavior of Qudo's work
+
+=head1 SYNOPSIS
+
+    package Myworker;
+    use base qw/ Qudo::Worker /;
+
+    sub work {
+        my ($self , $job ) = @_;
+
+        my $job_arg = $job->arg();
+        print "This is Myworker's work. job has argument == $job_arg \n";
+
+        $job->completed();
+    }
+    ### end of Myworker package.
+
+=head1 DESCRIPTION
+
+Qudo::Worker is based on all your work class of using Qudo.
+
+Your application have to inherit Qudo::Worker anyway.
+And it has to have 'work' method too.
+
+'work' method accept Qudo::Job object at parameter.
+If your work complete , you may call Qudo::Job->complete() method.
+
+=head1 REPOS
+
+http://github.com/nekokak/qudo/tree/master
+
+=head1 AUTHOR
+
+Atsushi Kobayashi <nekokak _at_ gmail dot com>
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
 1;
